46d48cfeafe4aa22d42349f2e956dd82e48ee17c
Thomas Garayua <tgarayua@gmail.com>
2023-07-21 21:00:57 -0400

    init Firebase configuration

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,22 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Thomas Garayua on 7/21/23.
+//
+
+import FirebaseCore
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
