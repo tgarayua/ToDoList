2a5dfb25f7c752fe38cff7059666cd59fbb047a1
Thomas Garayua <tgarayua@gmail.com>
2023-07-25 19:59:25 -0400

    Finished Lesson 10: Item List - Created the layout for the ToDoListItemView. Created the List of items in the ToDoListView

--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -5,20 +5,30 @@
 //  Created by Thomas Garayua on 7/21/23.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
     @StateObject var viewModel = ToDoListViewViewModel()
-    private let userId: String
+    @FirestoreQuery var items: [ToDoListItem]
     
     init(userId: String) {
-        self.userId = userId
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button("Delete") {
+                                viewModel.delete(id: item.id)
+                            }
+                            .background(Color.red)
+                        }
+                }
+                .listStyle(PlainListStyle())
             }
             .navigationTitle("To Do List")
             .toolbar {
@@ -38,6 +48,6 @@ struct ToDoListView: View {
 
 struct ToDoListView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListView(userId: "")
+        ToDoListView(userId: "96VNaiBN2dYRyDmf5xbL6M2ebat2")
     }
 }
