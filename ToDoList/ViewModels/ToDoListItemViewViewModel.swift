fbf8f0fd4b2c5b4cfbc23bba662d7306c10d1588
Thomas Garayua <tgarayua@gmail.com>
2023-07-26 00:54:52 -0400

    Finished Lesson 14: App Icon & Launch Screen

--- ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,9 +5,27 @@
 //  Created by Thomas Garayua on 7/21/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 /// ViewModel for a single to do list item view (each row in items list)
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
