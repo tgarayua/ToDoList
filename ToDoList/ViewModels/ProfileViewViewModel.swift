d8e63cec61a3285567ee68c1dd7a23bc67beb620
Thomas Garayua <tgarayua@gmail.com>
2023-07-25 20:33:48 -0400

    Finished Lesson 12: Done Checkmarks

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -5,12 +5,26 @@
 //  Created by Thomas Garayua on 7/21/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
